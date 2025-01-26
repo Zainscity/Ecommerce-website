9185f8dfe533cc10c797a0d7df4cbb349f5cb5f5
Zain ul Abideen <mzainh06@gmail.com>
2025-01-26 11:22:02 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,15 @@
-import Image from "next/image";
-import {CiUser} from 'react-icons/ci'
-import Navbar from "./components/navbar/page";
+
 import HeroSection from "./components/HeroSec/page";
-import { MdFeaturedPlayList } from "react-icons/md";
 import Featured from "./components/featuredproduct/page";
 import Toppicks from "./components/toppicks/page";
 import NewArrival from "./components/new-arrival/page";
 import Blog from "./components/blog/page";
 import Insta from "./components/insta/page";
 import Footer from "./components/footer/page";
-import Delivery from "./components/delivery/page";
 
 export default function Home() {
   return (
     <div>
-      {/* <Navbar/> */}
       <HeroSection/>
       <Featured/>
       <Toppicks/>
